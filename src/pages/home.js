d68b6edc18009c53c72961aae4cbbcbe97ee2754
Berenice Soto <beresoto.f@gmail.com>
2020-11-04 21:49:11 -0600

    agregando gethash y corrigiendo paginas

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,15 @@
 const Home = () => {
-    const view = `
+  const view = `
     <div class="Characters">
       <article class="Character-item">
         <a href="#/1/">
-          <img src="image" atl="name">
+          <img src="image" alt="name">
           <h2>Name</h2>
         </a>
       </article>
     </div>
-    `;
-    return view;
+  `;
+  return view;
 };
 
 export default Home;
