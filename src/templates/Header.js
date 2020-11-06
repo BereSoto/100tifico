6a400dbc9b7e26b11978b7b487005e04eeb57a28
Berenice Soto <beresoto.f@gmail.com>
2020-11-05 19:48:17 -0600

    pintando informacion de los personajes

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/>
+          <a href="/">
             100tifi.co
           </a>
         </h1>
@@ -16,7 +16,6 @@ const Header = () => {
     </div>
   `;
   return view;
-
 };
 
 export default Header;
