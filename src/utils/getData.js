78e5a4ff5172bfdfd742ae124d3f62b5a1a308b2
Berenice Soto <beresoto.f@gmail.com>
2020-11-05 19:37:29 -0600

    comenzando a traer personajes

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,2 +1,14 @@
 const API = 'https://rickandmortyapi.com/api/character/';
 
+const getData = async (id) => {
+    const apiURL = id ? `${API}${id}` : API;
+    try {
+        const responde = await fetch(apiURL);
+        const data = await responde.json();
+        return data;
+    } catch (error) {
+        console.log('Fetch Error', error);
+    };
+};
+
+export default getData;
