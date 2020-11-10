dc8e2ffefbed9d2a1b68bd0086ebbf6cdeb9bc47
Berenice Soto <beresoto.f@gmail.com>
2020-11-10 17:56:50 -0600

    agregando error 404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,17 @@
 const Error404 = () => {
     const view = `
       <div class="Error404">
-        <h2>Error 404</h2>
+        <h3>OH JEEZ, IT'S A</h3>
+      <div class="morty">
+        <div class="hair"></div>
+        <div class="ears"></div>
+        <div class="face"></div>
+        <div class="eyes"></div>
+        <div class="nose"></div>
+        <div class="mouth"></div>
+        <div class="lines"></div>
+      </div>
+      <h1 class="error">404 ERROR</h1>
       </div>
     `;
     return view;
